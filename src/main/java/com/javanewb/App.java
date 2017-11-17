b75698dbe82fc2cb9402c6d47f5ad2433eecedc4
CrowHyc <huangyisi_cd@shishike.com>
2017-11-17 14:54:03 +0800

    [Mod] update zk config update app config

--- src/main/java/com/javanewb/App.java
+++ src/main/java/com/javanewb/App.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * Hello world!
  */
-@SpringBootApplication(scanBasePackages = {"com.javanewb","com.keruyun"}, exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"com.javanewb"}, exclude = {DataSourceAutoConfiguration.class})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
